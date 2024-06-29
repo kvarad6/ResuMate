994071378ce92d862497230b5df44e091c3bd644
Varad Kulkarni <kvarad6@gmail.com>
2024-06-29 19:28:36 +0530

    ResuMate | Backend URLs updated

--- client/src/components/ResumeUpload.jsx
+++ client/src/components/ResumeUpload.jsx
@@ -20,7 +20,7 @@ const ResumeUpload = () => {
   const sendPDFForExtraction = async (formData) => {
     console.log(formData);
     const response = await axios.post(
-      "http://localhost:8000/upload_pdf",
+      "https://promptopedia.onrender.com/upload_pdf",
       formData,
       {
         headers: { "Content-Type": "multipart/form-data" },
